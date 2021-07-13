f80c9e3182be67fedc3254b4a63a1f8c7ccdfcca
Andrew Humphries <andrewhumphrieswork@outlook.com>
2021-07-13 07:02:23 -0500

    Finalized Logic in formData Module. Making it self contained and launching Game Display module

--- myScript.js
+++ myScript.js
@@ -1,76 +1,75 @@
 (Tic_Tac_ToeGame = () =>{
-    let newButton = document.getElementById('new-button');
 
-//     const gameBoardDisplay = ((player1 = 'Player 1', playerselection = 'X',player2 = 'Computer', computerselection = 'O') => {
+    const gameBoardDisplay = (player1 = 'Player 1', playerselection = 'X',player2 = 'Computer', computerselection = 'O') => {
 
-//         let selections = {player1:player1, playerselection:playerselection,player2:player2, computerselection:computerselection};
+        let selections = {player1:player1, playerselection:playerselection,player2:player2, computerselection:computerselection};
         
-//         let gameBoardArray = [0,1,2,3,4,5,6,7,8];
+        let gameBoardArray = [0,1,2,3,4,5,6,7,8];
 
-//         const cacheDom = (() => {
-//             let popupForm = document.getElementById('popupForm'),
-//             playArea = document.getElementById('play-area')
+        const cacheDom = (() => {
+            let popupForm = document.getElementById('popupForm'),
+            playArea = document.getElementById('play-area')
 
-//             return{popupForm:popupForm,playArea:playArea} 
-//         })()
+            return{popupForm:popupForm,playArea:playArea} 
+        })()
 
-//         const domElementMaker = (tag ,id="" ,cLass= "") => {
-//             let item = document.createElement(tag)
-//             item.id = id;
-//             item.className= cLass;
-//             return item;
-//         }
+        const domElementMaker = (tag ,id="" ,cLass= "") => {
+            let item = document.createElement(tag)
+            item.id = id;
+            item.className= cLass;
+            return item;
+        }
 
-//         const addDomElements = (array) =>{
+        const addDomElements = (array) =>{
 
-//             //3x3 Grid
-//             for(i=0; i < array.length;i++){
-//                 let title = domElementMaker('div', 'block_' + array[i], 'block');
-//                 cacheDom.playArea.append(title);
-//             }
+            //3x3 Grid
+            for(i=0; i < array.length;i++){
+                let title = domElementMaker('div', 'block_' + array[i], 'block');
+                cacheDom.playArea.append(title);
+            }
 
-//             // Winner Displays
-//             for(i=1; i < 3;i++){
-//                 let title = domElementMaker('h2', 'winner' + i, 'winner');
-//                 cacheDom.playArea.append(title);
-//             }
+            // Winner Displays
+            for(i=1; i < 3;i++){
+                let title = domElementMaker('h2', 'winner' + i, 'winner');
+                cacheDom.playArea.append(title);
+            }
 
-//             //Insert Reset button inbetween Winner Displays
-//             let resetButton = domElementMaker('button', 'reset',)
-//             resetButton.innerHTML = "Reset Board";
-//             let winner2 = document.getElementById('winner2');
-//             cacheDom.playArea.insertBefore(resetButton, winner2);
+            //Insert Reset button inbetween Winner Displays
+            let resetButton = domElementMaker('button', 'reset',)
+            resetButton.innerHTML = "Reset";
+            let winner2 = document.getElementById('winner2');
+            cacheDom.playArea.insertBefore(resetButton, winner2);
 
-//             //Creating Selections for Winner Tags
-//             let playerSelect = (player, selection) => {
-//                 let playerName = domElementMaker('p');
-//                 let playerselection = domElementMaker('p');
-//                 let score = domElementMaker('p');
-//                 playerName.innerHTML = player;
-//                 playerselection.innerHTML = selection;
-//                 score.innerHTML= 'Score: 0'
+            //Creating Selections for Winner Tags
+            let playerSelect = (player, selection) => {
+                let playerName = domElementMaker('p');
+                let playerselection = domElementMaker('p');
+                let score = domElementMaker('p');
+                playerName.innerHTML = player;
+                playerselection.innerHTML = selection;
+                score.innerHTML= 'Score: 0'
 
-//                 return[playerName,playerselection,score]
-//             }
+                return[playerName,playerselection,score]
+            }
 
-//             //Inserting player Selections into Winner Tags
-//             let playerInsertion = (array, id) => {
-//                 let winner = document.getElementById(id);
-//                 for(i=0;i<array.length;i++){
-//                     winner.append(array[i])
-//                 }
-//             }
+            //Inserting player Selections into Winner Tags
+            let playerInsertion = (array, id) => {
+                let winner = document.getElementById(id);
+                for(i=0;i<array.length;i++){
+                    winner.append(array[i])
+                }
+            }
 
-//             playerInsertion(playerSelect(selections.player1, selections.playerselection),'winner1')
-//             playerInsertion(playerSelect(selections.player2, selections.computerselection),'winner2')
+            playerInsertion(playerSelect(selections.player1, selections.playerselection),'winner1')
+            playerInsertion(playerSelect(selections.player2, selections.computerselection),'winner2')
             
-//         }
+        }
 
-//         addDomElements(gameBoardArray)
-//         return {domElementMaker:domElementMaker};
-// })()
+        addDomElements(gameBoardArray)
+        return {domElementMaker:domElementMaker};
+}
 
-    const newGameButton = () => {
+    const newGameButton = (() => {
 
         // cache DOM elements
         const cacheDom = (() => {
@@ -90,6 +89,7 @@
             
         }
 
+        // Functions List
         const checkforGrid = () => {
             if (cacheDom.playArea.hasChildNodes()){
                 alert("Game Already Started");
@@ -98,14 +98,11 @@
             }
         }
 
-        formData.render.openForm();
+        // formData.render.openForm();
         bindEvents()
-}
-
-
+})()
 
     const formData = (() =>{
-        let data = [];
 
           // cache DOM elements
         const cacheDom = (() => {
@@ -135,7 +132,8 @@
 
             cacheDom.sumbmitButton.addEventListener('click', (event) => {
                 event.preventDefault();
-                formData();
+                processFormData();
+                render.closeForm();
             })
 
             cacheDom.computerSelectSwitch.addEventListener('click', () => {
@@ -145,34 +143,38 @@
         }
 
         // Handle Form Submission Data
-        function formData() {
+        function processFormData() {
             data = [];
             if(cacheDom.playerName.value == ""){
-                data.push("Player 1")
+                data.push("Player 1");
             }else{
                 data.push(cacheDom.playerName.value);
-            }
+            };
 
-            if(cacheDom.playerCharSelection.checked == true){
-                data.push("X")
+            if(cacheDom.playerCharSelection.checked){
+                data.push("X");
             }else{
-                data.push("O")
-            }
+                data.push("O");
+            };
 
-            if(cacheDom.computerSelection.checked == false){
-                data.push("Computer")
+            if(!cacheDom.computerSelection.checked){
+                data.push("Computer");
             }else{
                 data.push(cacheDom.player2Name.value)
-            }
+            };
 
             if(data[1] == "X"){
-                data.push("O")
+                data.push("O");
             }else{
-                data.push("X")
-            }
+                data.push("X");
+            };
 
+            render.clearInput();
+            render.player2NameClose();
+            gameBoardDisplay(...data);
             console.log(data)
-        }    
+            return{data:data};
+        };    
 
         // render DOM
         const render = (()=> {
@@ -182,35 +184,37 @@
             const closeForm = () =>{
                 cacheDom.form.style.display = "none";
             }
-            const player2NameOpen = () => {
+            const player2NameClose = () => {
                 cacheDom.formContainer2.style.display = "none";
             }
-            const player2NameClose = () => {
+            const player2NameOpen = () => {
                 cacheDom.formContainer2.style.display = "grid";
                 cacheDom.player2Name.value = '';
             }
-            const clearPlayer1Input = () => {
-                cacheDom.playerName.value = ''
+            const clearInput = () => {
+                cacheDom.playerName.value = '';
+                cacheDom.player2Name.value = '';
+                cacheDom.computerSelection.checked = false;
+                cacheDom.playerCharSelection.checked;
             }
 
-            return{openForm:openForm, closeForm:closeForm, player2NameOpen:player2NameOpen, player2NameClose:player2NameClose, clearPlayer1Input:clearPlayer1Input}
+            return{openForm:openForm, closeForm:closeForm, player2NameOpen:player2NameOpen, player2NameClose:player2NameClose, clearInput:clearInput}
         })()
 
         const computerSelectionCheck = () =>{
-            if (cacheDom.computerSelection.checked == true){
-                render.player2NameClose();
-            }else{
+            if (cacheDom.computerSelection.checked){
                 render.player2NameOpen();
+            }else{
+                render.player2NameClose();
             }
         }
 
         bindEvents()
-        return{render:render ,data:data}
+        return{render:render}
 
 
 })()
     // formData.render.openForm()
-    newGameButton()
     
     // When new Player button is Clicked:
     // Board is Generated
